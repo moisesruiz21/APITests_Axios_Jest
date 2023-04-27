c625242b89722beaa6461c129624a213c719e5ab
Moisés Ruiz <40002039+moisesruiz21@users.noreply.github.com>
2023-04-27 17:48:31 -0600

    Update enviroment.ts

--- src/env/enviroment.ts
+++ src/env/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     API_URL: "https://api.trello.com/1",
-    TRELLO_API_KEY: "13d6999acc03fafed79c7525a20000ef",
-    TRELLO_OAUTH_TOKEN: "ATTA6f14ae215bea819c739b1d468033cc12851675cb622f31494ae69d6bd9ba572d3FF7DEE6",
+    TRELLO_API_KEY: "",
+    TRELLO_OAUTH_TOKEN: "",
 };
 
